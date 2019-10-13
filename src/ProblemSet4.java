c043e51e43fed8bd1eeff3abbf554a7f68111a3d
aravishankar <aryanravishankar02@gmail.com>
2019-10-12 20:55:01 -0400

    Third commit or something

--- src/ProblemSet4.java
+++ src/ProblemSet4.java
@@ -30,12 +30,12 @@ public class ProblemSet4 {
 //       ps.sum();
 //        ps.reverse();
 //        ps.digits();
-        ps.average();
-        ps.prime();
-        ps.fibonacci();
-        ps.factors();
-        ps.mario();
-        ps.luigi();
+//        ps.average();
+//        ps.prime();
+//        ps.fibonacci();
+//        ps.factors();
+//        ps.mario();
+//       ps.luigi();
         ps.credit();
                 
         in.close();
@@ -159,6 +159,27 @@ public class ProblemSet4 {
     
     public void average() {
 
+        double integer;
+        double total = 0;
+        double count = 0;
+
+        do 
+        {
+            System.out.print("Non-negative integer: ");
+            integer = in.nextLong();
+            if (integer > 0) {
+                total += integer;
+                count++;
+            }
+
+        } while (integer >= 0);
+        
+        // double newTotal = (Double)total;
+        // double newCount = (Double)count;
+        // double average = newTotal/newCount;
+        double average = total/count;
+        System.out.printf("\n%,.2f.", average);
+
     }
     
     /*
@@ -168,6 +189,34 @@ public class ProblemSet4 {
      */
     
     public void prime() {
+
+        boolean isPrime = true;
+        long integer;
+
+        do 
+        {
+            System.out.print("Non-negative integer: ");
+            integer = in.nextLong();
+
+        } while (integer < 0);
+
+        if (integer == 1) {
+            isPrime = true;
+        } else if (integer == 0) {
+            isPrime = false;
+        }
+        
+        for (int i = 2; i < integer; i++) {
+            if (integer % i == 0 && integer != i) {
+                isPrime = false;
+            }
+        }
+
+        if (isPrime) {
+            System.out.println("\nPrime.\n");
+        } else {
+            System.out.println("\nNot prime.\n");
+        }
 
     }
     
@@ -180,6 +229,26 @@ public class ProblemSet4 {
     
     public void fibonacci() {
 
+        long integer;
+        int prevInt = 0;
+        int currentInt = 1;
+        int nextInt = 1;
+
+        do 
+        {
+            System.out.print("Positive integer: ");
+            integer = in.nextLong();
+
+        } while (integer < 1 || integer > 92);
+
+        for (int i = 1; i < integer; i++) {
+            nextInt = prevInt + currentInt;
+            prevInt = currentInt;
+            currentInt = nextInt;
+        }
+
+        System.out.printf("\n%d.\n", nextInt);
+
     }
     
     /*
@@ -189,6 +258,27 @@ public class ProblemSet4 {
      */
     
     public void factors() {
+
+        long integer;
+
+        do 
+        {
+            System.out.print("Positive integer: ");
+            integer = in.nextLong();
+
+        } while (integer < 1);
+
+        System.out.printf("\n1, %d", integer);
+
+        for (int i = 2; i <= integer/i; i++) {
+
+            if (integer % i == 0) {
+                System.out.printf(", %d, %d", i, integer/i);
+            }
+
+        }
+
+        System.out.print(".\n");
 
     }
     
@@ -201,6 +291,36 @@ public class ProblemSet4 {
     
     public void mario() {        
 
+        long height;
+        long numSpace = 0;
+
+        do 
+        {
+            System.out.print("Height: ");
+            height = in.nextLong();
+
+        } while (height < 1 || height > 24);
+
+        System.out.print("\n");
+
+        for (long i = 1; i <= height; i++) {
+
+            for (long j = height-i; j > 0; j--) {
+                System.out.print(" ");
+                numSpace++;
+            }
+
+            for (long h = height+1-numSpace; h > 0; h--) {
+                System.out.print("#");
+            }
+
+            System.out.print("\n");
+            numSpace = 0;
+
+        }
+
+        System.out.print("\n");
+
     }
     
     /*
@@ -212,6 +332,45 @@ public class ProblemSet4 {
     
     public void luigi() {
 
+        long height;
+        long numSpace = 0;
+        long numHash = 0;
+
+        do 
+        {
+            System.out.print("Height: ");
+            height = in.nextLong();
+
+        } while (height < 1 || height > 24);
+
+        System.out.print("\n");
+
+        for (long i = 1; i <= height; i++) {
+
+            for (long j = height-i; j > 0; j--) {
+                System.out.print(" ");
+                numSpace++;
+            }
+
+            for (long h = height+1-numSpace; h > 0; h--) {
+                System.out.print("#");
+                numHash++;
+            }
+
+            System.out.print("  ");
+
+            for (long k = 0; k < numHash; k++) {
+                System.out.print("#");
+            }
+
+            System.out.print("\n");
+            numSpace = 0;
+            numHash = 0;
+
+        }
+
+        System.out.print("\n");
+
     }
     
     /*
@@ -222,6 +381,72 @@ public class ProblemSet4 {
      */
     
     public void credit() {
+        
+        long number;
+        long temp = 0;
+        long oddSum = 0;
+        long totalSum = 0;
+        String totalSumString;
+
+        do 
+        {
+            System.out.print("Number: ");
+            number = in.nextLong();
+
+        } while (number <= 0);
+
+        String numberString = Long.toString(number);
+
+        for (int i = 0; i < numberString.length(); i++) {
+            
+            if (i % 2 == 0) {
+                totalSum += Integer.parseInt(numberString.substring(i,i+1));
+            } else {
+
+                temp = Integer.parseInt(numberString.substring(i,i+1))*2;
+                // System.out.println(temp);
+
+                for (int j = 0; j < String.valueOf(temp).length(); j++) {
+                    oddSum += Integer.parseInt(String.valueOf(temp).substring(j, j+1));
+                }
+
+            }
+
+        }
+
+        // System.out.println(totalSum);
+        // System.out.println(oddSum);
+
+        totalSum += oddSum;
+        totalSumString = String.valueOf(totalSum);
+
+        if ((totalSumString.substring(totalSumString.length()-1, totalSumString.length())).equals("0")) {
+            if (numberString.length() == 15 && (numberString.substring(0,2).equals("34") || numberString.substring(0,2).equals("37"))) {
+                System.out.println("\nAmex.\n");
+            } else if (numberString.length() == 16 && (numberString.substring(0,2).equals("51") || numberString.substring(0,2).equals("52") || numberString.substring(0,2).equals("53") || numberString.substring(0,2).equals("54") || numberString.substring(0,2).equals("55"))) {
+                System.out.println("\nMastercard.\n");
+            } else if ((numberString.length() == 16 || numberString.length() == 13) && numberString.substring(0,1).equals("4")) {
+                System.out.println("\nVisa.\n");
+            } else {
+                System.out.println("\nInvalid.\n");
+            }
+        } else {
+            System.out.println("\nInvalid.\n");
+        }
+
+        // System.out.println(numberString.length());
+        // System.out.println(numberString.substring(0,2));
+
+
+
+
+
+        // System.out.println(totalSumString);
+        // boolean isTrue = totalSumString.substring(totalSumString.length()-1, totalSumString.length()) == "0";
+        // boolean isTrue2 = (totalSumString.substring(totalSumString.length()-1, totalSumString.length())).equals("0");
+        // System.out.println(isTrue);
+        // System.out.println(isTrue2);
+        // System.out.println(totalSumString.substring(totalSumString.length()-1, totalSumString.length()));
         
     }
 }
